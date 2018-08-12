d890cdbb596205bf15411c6b6d266d5ee0418ead
Jan Küster <jkuester@uni-bremen.de>
2018-08-12 13:23:15 +0200

    prevent TypeError on mispelled collections

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'ostrio:autoform-files',
   summary: 'File upload for AutoForm using ostrio:files',
   description: 'File upload for AutoForm using ostrio:files',
-  version: '2.1.1',
+  version: '2.1.2',
   git: 'https://github.com/VeliovGroup/meteor-autoform-file.git'
 });
 
