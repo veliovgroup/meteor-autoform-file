3ebfa9fe6d38343ab394ecbf2426eb398cc172b6
dr.dimitru <ceo@veliov.com>
2022-06-14 16:51:01 +0300

    📦 v2.5.0
    __Major changes:__
    
    - ⚠️ Minimum required version of meteor is `2.0`
    - ⚠️ Removed `global.Buffer = function () {};`
    
    __Changes:__
    
    - 🤝 Compatibility with `meteor@2.7.3`
    - 📦 `templating@1.4.2`, *was `v1.3.2`*
    - 📦 `ostrio:files@2.1.1`, *was `v2.0.0`*

--- package.js
+++ package.js
@@ -2,21 +2,21 @@ Package.describe({
   name: 'ostrio:autoform-files',
   summary: 'File upload for AutoForm using ostrio:files',
   description: 'File upload for AutoForm using ostrio:files',
-  version: '2.4.0',
+  version: '2.5.0',
   git: 'https://github.com/veliovgroup/meteor-autoform-file.git'
 });
 
 Package.onUse((api) => {
-  api.versionsFrom('METEOR@1.9');
+  api.versionsFrom('METEOR@2.0');
 
   api.use([
     'check',
     'ecmascript',
     'mongo',
     'reactive-var',
-    'templating@1.3.2',
+    'templating@1.4.2',
     'aldeed:autoform@7.0.0',
-    'ostrio:files@2.0.0'
+    'ostrio:files@2.1.1'
   ], 'client');
 
   api.addFiles([
